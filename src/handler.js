1c0e12cd058e3fb449f0f5fca60d4108d3be7e18
Tom Galligan <tomgalligan@Toms-MacBook-Pro.local>
2019-09-26 10:01:03 +0100

    add delete and change-status features

--- src/handler.js
+++ src/handler.js
@@ -3,45 +3,74 @@ const querystring = require("querystring");
 let toDoList = [];
 
 const handler = (request, response) => {
-    const endpoint = request.url;
+	const endpoint = request.url;
 
-    if (endpoint === "/get-list") {
-        console.log(toDoList);
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.end(`<h1>Successful GET request</h1>`);
-    } else if (endpoint === "/create-post") {
-        let allTheData = "";
+	if (endpoint === "/get-list") {
+		console.log(toDoList);
+		response.writeHead(200, { "Content-Type": "text/html" });
+		response.end(`<h1>Successful GET request</h1>`);
+	} else if (endpoint === "/create-post") {
+		let allTheData = "";
 
-        request.on("data", chunckOfData => {
-            allTheData += chunckOfData;
-        });
+		request.on("data", chunckOfData => {
+			allTheData += chunckOfData;
+		});
 
-        request.on("end", () => {
-            const postData = querystring.parse(allTheData);
+		request.on("end", () => {
+			const postData = querystring.parse(allTheData);
 
-            let newItem = {};
-            newItem.title = postData.title;
+			let newItem = {};
+			newItem.title = postData.title;
 
-            if (postData.status) {
-                newItem.status = postData.status;
-            } else {
-                newItem.status = false;
-            }
+			if (postData.status) {
+				newItem.status = postData.status;
+			} else {
+				newItem.status = false;
+			}
 
-            const dateNow = new Date();
-            newItem.dateCreated = dateNow.toUTCString();
-            newItem.dateEdited = dateNow.toUTCString();
+			const dateNow = new Date();
+			newItem.dateCreated = dateNow.toUTCString();
+			newItem.dateEdited = dateNow.toUTCString();
 
-            toDoList.push(newItem);
-            console.log(toDoList);
+			toDoList.push(newItem);
+			console.log(toDoList);
 
-            response.writeHead(200, { "Content-Type": "text/html" });
-            response.end("<h1>Successful POST request</h1>");
-        });
-    } else {
-        response.writeHead(404, { "Content-Type": "text/html" });
-        response.end("<h1>not create post endpoint</h1>"); // finish response
-    }
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.end("<h1>Successful POST request</h1>");
+		});
+	} else if (endpoint === "/delete-item") {
+		let allTheData = "";
+
+		request.on("data", chunckOfData => {
+			allTheData += chunckOfData;
+		});
+
+		request.on("end", () => {
+			const deleteInfo = querystring.parse(allTheData);
+			id = deleteInfo.id;
+			toDoList.splice(id, 1);
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.end("<h1>Successful deletion</h1>");
+		});
+	} else if (endpoint === "/change-status") {
+		let allTheData = "";
+
+		request.on("data", chunckOfData => {
+			allTheData += chunckOfData;
+		});
+
+		request.on("end", () => {
+			const requestInfo = querystring.parse(allTheData);
+			id = requestInfo.id;
+			newStatus = requestInfo.status;
+			toDoList[id].status = Boolean(newStatus);
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.end("<h1>Successful change of status</h1>");
+		});
+	} else {
+		response.writeHead(404, { "Content-Type": "text/html" });
+		response.end("<h1>not create post endpoint</h1>"); // finish response
+	}
 };
 
 module.exports = handler;
--- src/server.js
+++ src/server.js
@@ -2,15 +2,11 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const handler = require("./handler.js");
-const handlerPublic = require("./handlerPublic.js");
-const handlerHome = require("./handlerHome.js");
 
 const port = 3000;
 
 const server = http.createServer(handler);
 
 server.listen(port, function() {
-    console.log(
-        `Server is listening on port http://localhost:${port} . Ready to accept requests!`
-    );
+	console.log(`Server is listening on port http://localhost:${port} . Ready to accept requests!`);
 });
