4c2e310a068a1b73214cdaa1910357224da18152
TKostrzewski <thomas_kostrzewski@hotmail.com>
2019-09-26 13:28:05 +0100

    Modularise sorting function

--- src/handler.js
+++ src/handler.js
@@ -2,99 +2,113 @@ const querystring = require("querystring");
 
 let toDoList = [];
 
-const sort = (method, arr) => {
-	// method should be either dateCreated or dateEdited
-	const newArr = [...arr];
+const sortByDate = (method, arr) => {
+    // method should be either dateCreated or dateEdited
+    const newArr = [...arr];
 
-	newArr.sort((a, b) => {
-		const dateA = new Date(a[method]);
-		const dateB = new Date(b[method]);
-		return dateB - dateA;
-	});
+    newArr.sort((a, b) => {
+        const dateA = new Date(a[method]);
+        const dateB = new Date(b[method]);
+        return dateB - dateA;
+    });
 
-	return newArr;
+    return newArr;
+};
+
+const sortByStatus = arr => {
+    let trueArr = arr.filter(obj => {
+        obj.status === true;
+    });
+
+    let falseArr = arr.filter(obj => {
+        obj.status === false;
+    });
+
+    return [trueArr, falseArr];
 };
 
 const handler = (request, response) => {
-	const endpoint = request.url;
+    const endpoint = request.url;
 
-	if (endpoint === "/get-list") {
-		console.log(toDoList);
-		response.writeHead(200, { "Content-Type": "text/html" });
-		response.end(`<h1>Successful GET request</h1>`);
-	} else if (endpoint === "/create-post") {
-		let allTheData = "";
+    if (endpoint === "/get-list") {
+        console.log(toDoList);
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(`<h1>Successful GET request</h1>`);
+    } else if (endpoint === "/create-post") {
+        let allTheData = "";
 
-		request.on("data", chunckOfData => {
-			allTheData += chunckOfData;
-		});
+        request.on("data", chunckOfData => {
+            allTheData += chunckOfData;
+        });
 
-		request.on("end", () => {
-			const postData = querystring.parse(allTheData);
+        request.on("end", () => {
+            const postData = querystring.parse(allTheData);
 
-			let newItem = {};
-			newItem.title = postData.title;
+            let newItem = {};
+            newItem.title = postData.title;
 
-			if (postData.status) {
-				newItem.status = postData.status;
-			} else {
-				newItem.status = false;
-			}
+            if (postData.status) {
+                newItem.status = postData.status;
+            } else {
+                newItem.status = false;
+            }
 
-			const dateNow = new Date();
-			newItem.dateCreated = dateNow.toUTCString();
-			newItem.dateEdited = dateNow.toUTCString();
+            const dateNow = new Date();
+            newItem.dateCreated = dateNow.toUTCString();
+            newItem.dateEdited = dateNow.toUTCString();
 
-			toDoList.push(newItem);
-			console.log(toDoList);
+            toDoList.push(newItem);
+            console.log(toDoList);
 
-			response.writeHead(200, { "Content-Type": "text/html" });
-			response.end("<h1>Successful POST request</h1>");
-		});
-	} else if (endpoint === "/delete-item") {
-		let allTheData = "";
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.end("<h1>Successful POST request</h1>");
+        });
+    } else if (endpoint === "/delete-item") {
+        let allTheData = "";
 
-		request.on("data", chunckOfData => {
-			allTheData += chunckOfData;
-		});
+        request.on("data", chunckOfData => {
+            allTheData += chunckOfData;
+        });
 
-		request.on("end", () => {
-			const deleteInfo = querystring.parse(allTheData);
-			id = deleteInfo.id;
-			toDoList.splice(id, 1);
-			response.writeHead(200, { "Content-Type": "text/html" });
-			response.end("<h1>Successful deletion</h1>");
-		});
-	} else if (endpoint === "/change-status") {
-		let allTheData = "";
+        request.on("end", () => {
+            const deleteInfo = querystring.parse(allTheData);
+            id = deleteInfo.id;
+            toDoList.splice(id, 1);
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.end("<h1>Successful deletion</h1>");
+        });
+    } else if (endpoint === "/change-status") {
+        let allTheData = "";
 
-		request.on("data", chunckOfData => {
-			allTheData += chunckOfData;
-		});
+        request.on("data", chunckOfData => {
+            allTheData += chunckOfData;
+        });
 
-		request.on("end", () => {
-			const requestInfo = querystring.parse(allTheData);
-			id = requestInfo.id;
-			newStatus = requestInfo.status;
-			toDoList[id].status = Boolean(newStatus);
-			toDoList[id].dateEdited = new Date().toUTCString();
+        request.on("end", () => {
+            const requestInfo = querystring.parse(allTheData);
+            id = requestInfo.id;
+            newStatus = requestInfo.status;
+            toDoList[id].status = Boolean(newStatus);
+            toDoList[id].dateEdited = new Date().toUTCString();
 
-			response.writeHead(200, { "Content-Type": "text/html" });
-			response.end("<h1>Successful change of status</h1>");
-		});
-	} else if (endpoint.indexOf("sort") !== -1) {
-		const method = endpoint.split("=")[1]; // get method for sorting
-		if (method === "dateCreated") {
-			console.log(sort(dateCreated, toDoList));
-		} else if (method === "dateEdited") {
-			console.log(sort(dateEdited, toDoList));
-		}
-		response.writeHead(200, { "Content-Type": "text/html" });
-		response.end("<h1>Successful sort</h1>");
-	} else {
-		response.writeHead(404, { "Content-Type": "text/html" });
-		response.end("<h1>not create post endpoint</h1>"); // finish response
-	}
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.end("<h1>Successful change of status</h1>");
+        });
+    } else if (endpoint.indexOf("sort") !== -1) {
+        const method = endpoint.split("=")[1]; // get method for sorting
+        if (method === "dateCreated" || method === "dateEdited") {
+            const sortedToDoList = sortByDate(method, toDoList);
+            console.log(sortedToDoList);
+        } else if (method === "status") {
+            [trueItems, falseItems] = sortByStatus(toDoList);
+            console.log(trueItems, falseItems);
+        }
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end("<h1>Successful sort</h1>");
+    } else {
+        response.writeHead(404, { "Content-Type": "text/html" });
+        response.end("<h1>not create post endpoint</h1>"); // finish response
+    }
 };
 
 module.exports = handler;
