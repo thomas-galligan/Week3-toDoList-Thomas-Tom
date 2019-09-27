9a50642660d422a86b7b3eea649c4daca9037aca
TKostrzewski <thomas_kostrzewski@hotmail.com>
2019-09-27 15:17:25 +0100

    Clarify handler and router Relates #19

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const functions = require("./handler.js");
+const router = require("./router.js");
 
 const port = 3000;
 
-const server = http.createServer(functions.handler);
+const server = http.createServer(router);
 
 server.listen(port, function() {
-	console.log(`Server is listening on port http://localhost:${port} . Ready to accept requests!`);
+    console.log(
+        `Server is listening on port http://localhost:${port} . Ready to accept requests!`
+    );
 });
