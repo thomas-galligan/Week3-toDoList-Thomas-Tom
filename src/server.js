5a195cf4a67c50ffde4f90c6d19a9a203ffef79a
Tom Galligan <tomgalligan@Toms-MacBook-Pro.local>
2019-09-27 10:08:27 +0100

    add testing

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const handler = require("./handler.js");
+const functions = require("./handler.js");
 
 const port = 3000;
 
-const server = http.createServer(handler);
+const server = http.createServer(functions.handler);
 
 server.listen(port, function() {
 	console.log(`Server is listening on port http://localhost:${port} . Ready to accept requests!`);
